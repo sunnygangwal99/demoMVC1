2553c3d9a924e9fdfe3dfddb8f4f9aa3cae211bc
Sunny Gangwal <sunny.gangwal@nagarro.com>
2020-03-24 16:15:26 +0530

    javahello

---
+++ src/test/java/demoMVC1/TestHelloWorld.java
@@ -0,0 +1,13 @@
+package demoMVC1;
+
+import org.testng.annotations.Test;
+
+public class TestHelloWorld {
+	
+	@Test
+	public void testHelloWorld() {
+		
+		System.out.println("..Hello world");
+	}
+
+}
